4889adb19e4733cb7b4f917fd5bfe56d25634196
PauloHenriqueOC <paulohoc1@gmail.com>
2024-05-03 10:44:25 -0300

    router adjustment

--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,28 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Home from './pages/home/Home.jsx'
-import QrCode from './pages/fe-mentor-qrcode/QrCode.jsx'
+import {
+	createHashRouter,
+	RouterProvider,
+} from "react-router-dom";
+import Home from "./pages/home/Home.jsx";
+import QrCode from "./pages/fe-mentor-qrcode/QrCode.jsx";
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Home/>
-  },
+const router = createHashRouter([
+	{
+		path: "/",
+		element: <Home />,
+	},
 
-  {
-    path: 'fe-mentor/qr-code',
-    element: <QrCode />
-  }
-])
+	{
+		path: "fe-mentor/qr-code",
+		element: <QrCode />,
+	},
+]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<React.StrictMode>
+		<RouterProvider router={router} />
+	</React.StrictMode>
+);
